83c172c7845a127b5452cbd9c31876e4045b5492
Henrik <dev@ubuntu.(none)>
2012-12-23 00:14:00 +0100

    as

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -2,7 +2,7 @@
 $(function () {
 var now = new Date();
 
-    $('#entry_start_pit, #entry_end_pit').mobiscroll().datetime({
+    $('#entry_start_pit').mobiscroll().datetime({
         minDate: new Date(now.getFullYear(), now.getMonth(), now.getDate()),
         theme: 'android-ics light',
         lang: 'de',
