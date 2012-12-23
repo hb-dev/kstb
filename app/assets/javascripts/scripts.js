b844562e2c225c3d9461bc98c66545ecebf1c2b0
Henrik <dev@ubuntu.(none)>
2012-12-23 02:47:28 +0100

    as

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,12 +1,1 @@
 
-$(function () {
-var now = new Date();
-
-    $('#entry_start_pit, #entry_end_pit').mobiscroll().datetime({
-        minDate: new Date(now.getFullYear(), now.getMonth(), now.getDate()),
-        theme: 'android-ics light',
-        lang: 'de',
-        display: 'modal',
-        mode: 'mixed'
-    });  	
-})
